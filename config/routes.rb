3a83b2730b2ac4e1a91e07a1d47b4e6df30ded8b
Tamae <1am1goodtime@gmail.com>
2021-08-06 09:18:00 +0000

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+  root to: 'homes#top'
+  get 'homes/about'
+
+  resources :user_infos, only: [:show, :edit, :update]
+
+  # member= resourcesでは自動生成されないものに使う。生成するroutingに:idがつく。
+  # collection= resourcesでは自動生成されないものに使う。生成するroutingに:idがつかない。
+  resources :tweets do
+    member do
+      post 'to_log'
+    end
+
+    collection do
+      get 'complete'
+    end
+  end
+  # post=URLが保存可、get=URL保存不可
+
 end
---
+++ test/controllers/user_infos_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class UserInfosControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get user_infos_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get user_infos_edit_url
+    assert_response :success
+  end
+
+end
