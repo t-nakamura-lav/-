3997fcc540d4734c4ad8b0c47bb13a51e8ef1d06
Tamae <1am1goodtime@gmail.com>
2021-08-08 07:43:05 +0000

    mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
-  def show
+
+ def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    # binding.pry
+    if @user.update(user_params)
+      flash[:success] = "登録情報を変更しました"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :profile_image, :introduction, :age_status, :job_status, :challenge_contents)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b5f08656b31a2a626beb98a80dd7bc2abe1d9db641fa7bf538911452ef7c7b6dd59d7e9049ce8b1bd09811b7209fcd799100a593c2b3c8787449899a29c063ec'
