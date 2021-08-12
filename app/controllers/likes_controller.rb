090248a9401ec40ea6236d9c29a4ab0dffe564bd
Tamae <1am1goodtime@gmail.com>
2021-08-12 08:51:48 +0000

    tweet/show_Like機能追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,17 @@
 class LikesController < ApplicationController
 
   def create
+    tweet = Tweet.find(params[:tweet_id])
+    like = current_user.likes.new(tweet_id: tweet.id)
+    like.save
+    redirect_to tweet_path(tweet)
   end
 
   def destroy
+    tweet = Tweet.find(params[:tweet_id])
+    like = current_user.likes.find_by(tweet_id: tweet.id)
+    like.destroy
+    redirect_to tweet_path(tweet)
   end
 
 end
