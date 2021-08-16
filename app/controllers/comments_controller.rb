bc86b193f3047aa2f99d5541b1e52aa67ceddca6
Tamae <1am1goodtime@gmail.com>
2021-08-16 09:37:18 +0000

    Rubocop導入

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,7 @@ class CommentsController < ApplicationController
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:comment)
-  end
+    def comment_params
+      params.require(:comment).permit(:comment)
+    end
 end
