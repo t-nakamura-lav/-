23ea56b08ba0954b4bdf59c7e9765730739e6e5b
Tamae <1am1goodtime@gmail.com>
2021-08-16 15:40:18 +0000

    コメント非同期通信

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,21 @@
 class CommentsController < ApplicationController
 
   def create
-    tweet = Tweet.find(params[:tweet_id])
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = Comment.new
     # comment = Comment.new(comment_params)と
     # comment.user_id = current_user.idの省略形 ↓
     comment = current_user.comments.new(comment_params)
-    comment.tweet_id = tweet.id
+    comment.tweet_id = @tweet.id
     comment.save
-    redirect_to tweet_path(tweet)
+    # redirect_to tweet_path(tweet) 非同期通信化
   end
 
   def destroy
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = Comment.new
     Comment.find_by(id: params[:id], tweet_id: params[:tweet_id]).destroy
-    redirect_to tweet_path(params[:tweet_id])
+    # redirect_to tweet_path(params[:tweet_id]) 非同期通信化
   end
 
 
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
     like = current_user.likes.new(tweet_id: @tweet.id)
     like.save
     # redirect_to tweet_path(tweet) 非同期通信化
-    # app/views/likes/create.js.erbを参照する
+    # app/views/likes/create.js.erbを参照
   end
 
   def destroy
@@ -13,7 +13,7 @@ class LikesController < ApplicationController
     like = current_user.likes.find_by(tweet_id: @tweet.id)
     like.destroy
     # redirect_to tweet_path(tweet) 非同期通信化
-    # app/views/likes/destroy.js.erbを参照する
+    # app/views/likes/destroy.js.erbを参照
   end
 
 end
