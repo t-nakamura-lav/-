afc846d95792a356e9c1dcc10a3de52161e99081
Tamae <1am1goodtime@gmail.com>
2021-08-17 10:51:35 +0000

    tweet/index,users/showいいね非同期通信

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,8 +2,6 @@ class TweetsController < ApplicationController
   def index
     # 投稿を新着順＆ページング
     @tweets = Tweet.all.order("id ASC").page(params[:page]).reverse_order
-    # @tweet.user_id = current_user.id
-    # @tweet = Tweet.find(params[:id])
   end
 
   def show
