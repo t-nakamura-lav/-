ec3e678a43f4d7c6afb2d5d39526d5449f71eb49
Tamae <1am1goodtime@gmail.com>
2021-08-19 11:54:24 +0000

    フォロー/フォロワーレイアウト補正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,5 @@
 class RelationshipsController < ApplicationController
-  
+
   # ——————フォロー機能を作成・保存・削除する————————————
   def create
     current_user.follow(params[:user_id])
@@ -8,17 +8,17 @@ class RelationshipsController < ApplicationController
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
+    redirect_to request.referer
   end
-  
+
 #————————フォロー・フォロワー一覧を表示する-————————————
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings
   end
 
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
-  end  
+    @user = User.find(params[:user_id])
+    @users = @user.followers
+  end
 end
