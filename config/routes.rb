2376d9dbeb6c49164c9f1ccf8ce69a8875cc1837
Tamae <1am1goodtime@gmail.com>
2021-08-31 11:04:11 +0000

    削除機能補正

--- config/routes.rb
+++ config/routes.rb
@@ -40,5 +40,5 @@ Rails.application.routes.draw do
   end
   # post=URLが保存不可、get=URL保存可
 
-  resources :calendars, only: [:create, :index, :edit, :destroy]
+  resources :calendars, only: [:create, :index, :destroy]
 end
