c35162211584c4efc5de7d24f2236c940312ce7f
Tamae <1am1goodtime@gmail.com>
2021-09-07 07:56:39 +0000

    通知作成メソッドの呼び出し

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,8 @@ class CommentsController < ApplicationController
     comment.tweet_id = @tweet.id
     comment.save
     # redirect_to tweet_path(tweet) 非同期通信化
+    # ↓通知作成メソッドの呼び出し
+    @tweet.create_notification_comment!(current_user, @comment.id)
   end
 
   def destroy
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,15 @@
 class LikesController < ApplicationController
   def create
-    # @tweet_like = @user.tweet
     @tweet = Tweet.find(params[:tweet_id])
     like = current_user.likes.new(tweet_id: @tweet.id)
     like.save
     # redirect_to tweet_path(tweet) 非同期通信化
     # app/views/likes/create.js.erbを参照
+    # ↓通知作成メソッドの呼び出し
+    @tweet.create_notification_like!(current_user)
   end
 
   def destroy
-    # @tweet_like = @user.tweet
     @tweet = Tweet.find(params[:tweet_id])
     like = current_user.likes.find_by(tweet_id: @tweet.id)
     like.destroy
