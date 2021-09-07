1e96fceca0f27f96a7b348142a22573ad73bb3e7
Tamae <1am1goodtime@gmail.com>
2021-09-07 07:46:38 +0000

    通知機能_フォロー通知の作成メソッド

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,17 @@ class User < ApplicationRecord
   # 通知機能
   has_many :active_notifications, class_name: "Notification", foreign_key: "visiter_id", dependent: :destroy
   has_many :passive_notifications, class_name: "Notification", foreign_key: "visited_id", dependent: :destroy
-
+  def create_notification_follow!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+      )
+      notification.save if notification.valid?
+    end
+  end
+ 
   validates :name, length: { maximum: 20 }
   validates :introduction, length: { maximum: 100 }
 
